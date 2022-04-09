042341b85e1e5a8e040cf126b7ff61ec18f0b282
Giu13sse <giussedipietro@gmail.com>
2022-04-08 19:24:23 -0500

    shell

---
+++ shell.c
@@ -0,0 +1,17 @@
+#main.h
+
+/**
+*main - read and execute the command that typed- infinite loop
+*@ac:argument counter
+*@av:argument vector
+*@env:envt vector
+*
+*Return:[2~[2~0
+*/[B
+
+int main(int ac, char **argvct[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[A[D[D[D[D[B[D[D[C[D[rgvct:argument vector[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[rgcont:argument counter[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[D[D[D[D, char *[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Drgc[A[A[A[A[A[A[D[D[D[D[D[D[D[D[D[A[B[D[[2~[C[D[3~[3~[nt:argument counter [B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[Dnt, char argvct[D[D[D[D[D[D[D **argvct, char *envt[])
+{}[D
+[D[A[C[
+
+
+}[A[A[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Dt:enviro[C[Coment vector[B[B[B[B[B[B[B[B[B[B[B
