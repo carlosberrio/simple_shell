600e80bf67c661ae4721e50e954050beb25ce20f
Carlos Berrio <4634@holbertonschool.com>
2022-04-13 14:31:39 -0500

    new version

--- testpath.c
+++ testpath.c
@@ -11,4 +11,33 @@
 char *testpath(char **token)
 {
     
+int r = 0, k = 0;
+char *path = NULL, *path_new = NULL, **tokenized_path = NULL;
+for (r = 0; environ[r]; r++)
+{
+if (_strncmp(environ[r], "PATH", 4) != NULL)
+{
+path = _strdup(environ[r]);
+break;
+}
+}
+if (path == NULL)
+return (NULL);
+tokenized_path = tokenizer(path, "=:");
+for (k = 1; tokenized_path[j]; k++)
+{
+path_new = _strdup(tokenized_path[k]);
+_strcat(path_new, "/");
+_strcat(path_new, tokens[0]);
+if (access(path_new, F_OK) == 0)
+{
+free(path);
+free(tokenized_path);
+return (path_new);
+}
+free(path_new);
+}
+free(path);
+free(tokenized_path);
+return (NULL);
 }
--- tokenizer.c
+++ tokenizer.c
@@ -4,10 +4,31 @@
  * tokenizer - Function that split a string.
  * @s: string to split.
  * @worde: words delimiter.
- * Return: 
+ * Return: separated words
  */
 
 char **tokenizer(char *str, char *worde)
 {
-    
+int i = 0, cow = 0;
+char **separated_words = NULL;
+cow = count_of_words(s);
+
+separated_words = malloc(sizeof(char *) * (cow + 1));
+if (separated_words == NULL)
+{
+perror("Error");
+return (NULL);
+}
+separated_words[0] = strtok(s, worde);
+if (splited_words[0] == NULL)
+{
+free(separated_words[0]);
+free(separated_words);
+return (NULL);
+}
+for (i = 1; i < cow; i++)
+{
+separated_words[i] = strtok(NULL, worde);
+}
+return (separated_words);
 }
