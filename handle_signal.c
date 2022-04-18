6447ada3d6677758432e7f7143bbfa5c1cb010e0
Giu13sse <giussedipietro@gmail.com>
2022-04-17 19:34:06 -0500

    add keys

--- handle_signal.c
+++ handle_signal.c
@@ -11,4 +11,4 @@ char handlesignal(int l)
 (void)l;
 write(STDERR_FILENO, "\n", 1);
 write(STDERR_FILENO, "$ ", 2);
-
+}
--- prompt_user.c
+++ prompt_user.c
@@ -10,7 +10,7 @@ char prompt_user(void)
 {
 if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1)){
 flags.interactive = 1;
-
+}
 if (flags.interactive)
 {
 write(STDERR_FILENO, "$ ", 2);
