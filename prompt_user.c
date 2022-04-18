23e12daae05d9251e5cea29bebdbe3b9716972e1
Giu13sse <giussedipietro@gmail.com>
2022-04-17 19:31:46 -0500

    fixing git errors

--- prompt_user.c
+++ prompt_user.c
@@ -10,14 +10,12 @@ char prompt_user(void)
 {
 if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1)){
 flags.interactive = 1;
-<<<<<<< HEAD
+
 if (flags.interactive)
 {
 write(STDERR_FILENO, "$ ", 2);
-=======
 }
 if (flags.interactive){
 write(STDERR_FILENO, "$ ", 2);
 }
->>>>>>> be31d2edea94d6d9b757d8c2a36b57f3cd78e376
 }
